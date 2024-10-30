bc07068b55ef4018fea25aa537fcb85fda62949b
BilalSheikh08 <roundedcrux08@gmail.com>
2024-10-30 11:12:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function showMoreWishes() {
+    alert("May the beauty of Diwali fill your world with happiness and positivity!");
+}
+
+function openVideoPage() {
+    window.open("video.html", "_blank");
+}
